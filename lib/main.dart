2b623167114cfaa8e0efdd939e6522a7cb4ad223
Ardan-213 <dutaardan635@gmail.com>
2025-03-11 22:33:01 +0700

    App Bar Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Login Page',
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page')
-      ),
-    )
-  );
+    return MaterialApp(
+      title: 'login page',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('login page'),
+        ),
+      )
+    );
   }
 }
